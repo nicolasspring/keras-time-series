53b845c8dfc0b0ff60c9b098c7269357ba829ce9
nicolasspring <springn.ch@gmail.com>
2023-02-23 18:31:00 +0100

    add FordA dataloader

--- kts/data_loaders/timeseries/base.py
+++ kts/data_loaders/timeseries/base.py
@@ -1,12 +1,35 @@
 import typing as t
+from abc import ABC, abstractmethod
+
+import numpy as np
 
 
-class BaseDataLoader:
+class BaseDataLoader(ABC):
     def __init__(self) -> None:
+        self.x_train: int
+        self.y_train: int
+        self.x_test: int
+        self.y_test: int
+        self.x_valid: int
+        self.y_valid: int
+
+    @abstractmethod
+    def _get_train_set(self) -> t.Tuple[np.ndarray, np.ndarray]:
         pass
 
-    def get_train_set(self):
+    @abstractmethod
+    def _get_test_set(self) -> t.Tuple[np.ndarray, np.ndarray]:
         pass
 
-    def get_test_set(self):
+    @abstractmethod
+    def _get_valid_set(self) -> t.Tuple[np.ndarray, np.ndarray]:
         pass
+
+    def get_train_set(self) -> t.Tuple[np.ndarray, np.ndarray]:
+        return self._get_train_set()
+
+    def get_test_set(self) -> t.Tuple[np.ndarray, np.ndarray]:
+        return self._get_test_set()
+
+    def get_valid_set(self) -> t.Tuple[np.ndarray, np.ndarray]:
+        return self._get_valid_set()
--- kts/data_loaders/timeseries/forda.py
+++ kts/data_loaders/timeseries/forda.py
@@ -1,9 +1,35 @@
+import typing as t
+
+import numpy as np
+
 from kts.data_loaders.timeseries.base import BaseDataLoader
 
 
 class FordADataLoader(BaseDataLoader):
+    """
+    data loader for the FordA dataset. heavily inspired by the Keras tutorial:
+    https://keras.io/examples/timeseries/timeseries_classification_from_scratch/
+    """
+
     def __init__(self) -> None:
         super().__init__()
         self.root_url = (
             "https://raw.githubusercontent.com/hfawaz/cd-diagram/master/FordA/"
         )
+        self.x_train, self.y_train = self.read_ford_a(f"{self.root_url}FordA_TRAIN.tsv")
+        self.x_test, self.y_test = self.read_ford_a(f"{self.root_url}FordA_TEST.tsv")
+
+    def read_ford_a(self, filename: str) -> t.Tuple[np.ndarray, np.ndarray]:
+        self.data = np.loadtxt(filename)
+        y = self.data[:, 0]
+        x = self.data[:, 1:]
+        return x, y.astype(int)
+
+    def _get_train_set(self) -> t.Tuple[np.ndarray, np.ndarray]:
+        return self.x_train, self.y_train
+
+    def _get_test_set(self) -> t.Tuple[np.ndarray, np.ndarray]:
+        self.x_test, self.y_test
+
+    def _get_valid_set(self):
+        raise NotImplementedError("FordA dataset does not contain a validation set")
